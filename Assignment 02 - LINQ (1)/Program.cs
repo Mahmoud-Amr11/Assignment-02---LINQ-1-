0b0d3623263d05820c53532f57da9008e73fe67d
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-08-15 15:21:42 +0300

    LINQ - Restriction Operators Q2

--- Assignment 02 - LINQ (1)/Program.cs
+++ Assignment 02 - LINQ (1)/Program.cs
@@ -13,7 +13,17 @@ namespace Assignment_02___LINQ__1_
             #region LINQ - Restriction Operators
             #region 1. Find all products that are out of stock.
 
-            var result = products.Where(p => p.UnitsInStock == 0);
+            //var result = products.Where(p => p.UnitsInStock == 0);
+            //foreach (var product in result)
+            //{
+            //    Console.WriteLine(product.ToString());
+            //}
+
+            #endregion
+
+            #region 2. Find all products that are in stock and cost more than 3.00 per unit.
+
+            var result = products.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3.00m);
             foreach (var product in result)
             {
                 Console.WriteLine(product.ToString());
