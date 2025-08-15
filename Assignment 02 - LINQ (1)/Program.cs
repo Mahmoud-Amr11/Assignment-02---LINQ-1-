4383fa69498c05e2d5950a79b05033a7e2b1832d
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-08-15 15:14:54 +0300

    Add project files.

---
+++ Assignment 02 - LINQ (1)/Program.cs
@@ -0,0 +1,16 @@
+﻿using ASSLINQ;
+
+namespace Assignment_02___LINQ__1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+
+
+
+
+        }
+    }
+}
