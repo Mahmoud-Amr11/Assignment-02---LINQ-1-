144374c37b0ad4107d3fcb10ce18dabe43e6548b
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-08-15 16:00:42 +0300

    LINQ - Aggregate Operators Q12

--- Assignment 02 - LINQ (1)/Program.cs
+++ Assignment 02 - LINQ (1)/Program.cs
@@ -182,24 +182,35 @@ namespace Assignment_02___LINQ__1_
             #endregion
 
             #region 11. Get the products with the cheapest price in each category (Use Let)
-            var cheapestProductsByCategory =
-            from p in products
-            group p by p.Category into g
-            let minPrice = g.Min(x => x.UnitPrice)
-            from prod in g
-            where prod.UnitPrice == minPrice
-            select new
-            {
-                Category = g.Key,
-                ProductName = prod.ProductName,
-                Price = prod.UnitPrice
-            };
+            //var cheapestProductsByCategory =
+            //from p in products
+            //group p by p.Category into g
+            //let minPrice = g.Min(x => x.UnitPrice)
+            //from prod in g
+            //where prod.UnitPrice == minPrice
+            //select new
+            //{
+            //    Category = g.Key,
+            //    ProductName = prod.ProductName,
+            //    Price = prod.UnitPrice
+            //};
 
-            foreach (var item in cheapestProductsByCategory)
+            //foreach (var item in cheapestProductsByCategory)
+            //{
+            //    Console.WriteLine($"{item.Category} - {item.ProductName} - {item.Price}");
+            //}
+
+            #endregion
+
+            #region 12. Get the most expensive price among each category's products.
+             
+            var result = products
+                .GroupBy(p => p.Category)
+                .Select(g => new { Category = g.Key, MostExpensivePrice = g.Max(p => p.UnitPrice) });
+            foreach (var item in result)
             {
-                Console.WriteLine($"{item.Category} - {item.ProductName} - {item.Price}");
+                Console.WriteLine($"{item.Category} - Most Expensive Price: {item.MostExpensivePrice:C}");
             }
-
             #endregion
 
 
